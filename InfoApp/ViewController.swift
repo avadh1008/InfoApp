7fffc3c4e4db5ad9364e4288505722b5fbe51762
Avadh Mewada <avadh.mewada@aimsinfosoft.com>
2020-06-19 11:13:17 +0530

    Initial Commit

---
+++ InfoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfoApp
+//
+//  Created by Avadh Mewada on 6/19/20.
+//  Copyright © 2020 Avadh Mewada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
