ebb7b732b61c600ce637a5fe00539f9d102aba4e
avadh <avadh@appmatictech.com>
2021-04-12 21:22:11 +0530

    - initial commit

---
+++ Avadh_Demo_Project/Avadh_Demo_Project/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Avadh_Demo_Project
+//
+//  Created by Avadh Mevada on 12/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
